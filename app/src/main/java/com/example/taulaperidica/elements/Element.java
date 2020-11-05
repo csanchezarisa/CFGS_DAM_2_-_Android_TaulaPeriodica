c3ba194911f3570ea77f357bbb406793086b047e
csanchezarisa <cristobal.sanchez@mataro.epiaedu.cat>
2020-11-05 14:00:19 +0100

    Constructor clase Element acabado e inicialización de los elementos en la MainActivity

--- app/src/main/java/com/example/taulaperidica/elements/Element.java
+++ app/src/main/java/com/example/taulaperidica/elements/Element.java
@@ -1,8 +1,24 @@
 package com.example.taulaperidica.elements;
 
 public class Element {
-    private String nom;
     private String simbol;
-    private int numero;
-    private String serieQuimica;
+    private String tipus;
+    private int numeroAtomic;
+    private String nom;
+    private String massaAtomica;
+    private String configuracioElectronica;
+    private String estatPredeterminat;
+
+    public Element(String simbol, int numeroAtomic, String tipus, String nom, String massaAtomica, String configuracioElectronica, String estatPredeterminat) {
+
+        this.simbol = simbol;
+        this.tipus = tipus;
+        this.numeroAtomic = numeroAtomic;
+        this.nom = nom;
+        this.massaAtomica = massaAtomica;
+        this.configuracioElectronica = configuracioElectronica;
+        this.estatPredeterminat = estatPredeterminat;
+
+    }
+
 }
