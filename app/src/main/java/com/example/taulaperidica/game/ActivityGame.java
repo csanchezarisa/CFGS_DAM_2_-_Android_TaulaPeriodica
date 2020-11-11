7d48239196685a230fbb834086734bb21bc5e114
csanchezarisa <cristobal.sanchez@mataro.epiaedu.cat>
2020-11-11 17:19:19 +0100

    Empezada logica del juego

--- app/src/main/java/com/example/taulaperidica/game/ActivityGame.java
+++ app/src/main/java/com/example/taulaperidica/game/ActivityGame.java
@@ -7,13 +7,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.taulaperidica.R;
 
 public class ActivityGame extends AppCompatActivity {
 
+    // Array que emmagatsema les puntuacions màximes
     private int[] puntuacionsMaximes;
+
+    // Constants que s'utilitzen per diferenciar entre els diferents jocs
+    private final int GAME_ENCERTAR_SIMBOL = 0;
+    private final int GAME_ENCERTAR_NOM = 1;
+    private final int GAME_ENCERTAR_ESTAT = 2;
+    private final int GAME_ENCERTAR_NUMERO = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,41 @@ public class ActivityGame extends AppCompatActivity {
         // Es recuperan les puntuacions màximes que envía la MainActivity
         puntuacionsMaximes = getIntent().getExtras().getIntArray("puntuacionsMaximes");
 
+        // Mostra les puntuacions en els TextView del Layout
+        actualitzarTextPuntuacions();
+
+        // Crear listeners als botons
+        Button btnEncertarSimbol = (Button) findViewById(R.id.btnStartEncertarSimbol);
+        btnEncertarSimbol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciarJoc(GAME_ENCERTAR_SIMBOL);
+            }
+        });
+
+        Button btnEncertarNom = (Button) findViewById(R.id.btnStartEncertarNom);
+        btnEncertarNom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciarJoc(GAME_ENCERTAR_NOM);
+            }
+        });
+
+        Button btnEncertarEstat = (Button) findViewById(R.id.btnStartEncertarEstat);
+        btnEncertarEstat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciarJoc(GAME_ENCERTAR_ESTAT);
+            }
+        });
+
+        Button btnEncertarNumero = (Button) findViewById(R.id.btnStartEncertarNumero);
+        btnEncertarNumero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciarJoc(GAME_ENCERTAR_NUMERO);
+            }
+        });
 
     }
 
@@ -42,6 +86,48 @@ public class ActivityGame extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // Escriu les puntuacions en els TextView del layout
+    private void actualitzarTextPuntuacions() {
+
+        TextView textView = (TextView) findViewById(R.id.txtPuntuacioEncertarSimbol);
+        textView.setText("Puntuació màxima: " + puntuacionsMaximes[GAME_ENCERTAR_SIMBOL]);
+
+        textView = (TextView) findViewById(R.id.txtPuntuacioEncertarNom);
+        textView.setText("Puntuació màxima: " + puntuacionsMaximes[GAME_ENCERTAR_NOM]);
+
+        textView = (TextView) findViewById(R.id.txtPuntuacioEncertarEstat);
+        textView.setText("Puntuació màxima: " + puntuacionsMaximes[GAME_ENCERTAR_ESTAT]);
+
+        textView = (TextView) findViewById(R.id.txtPuntuacioEncertarNumero);
+        textView.setText("Puntuació màxima: " + puntuacionsMaximes[GAME_ENCERTAR_NUMERO]);
+
+    }
+
+    private void iniciarJoc(int modalitat) {
+
+        switch (modalitat) {
+
+            case GAME_ENCERTAR_SIMBOL:
+                break;
+
+            case GAME_ENCERTAR_NOM:
+                break;
+
+            case GAME_ENCERTAR_ESTAT:
+                break;
+
+            case GAME_ENCERTAR_NUMERO:
+                break;
+
+            default:
+                return;
+
+        }
+
+
+
     }
 
     // Mètode que s'encarrega de finalitzar l'activity, retornant la puntuació
---
+++ app/src/main/java/com/example/taulaperidica/game/modalitats/ActivityGameEncertarSimbol.java
@@ -0,0 +1,14 @@
+package com.example.taulaperidica.game.modalitats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityGameEncertarSimbol extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_encertar_simbol);
+    }
+}
