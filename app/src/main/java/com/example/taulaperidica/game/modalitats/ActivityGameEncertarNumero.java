76c989e4b354c6419d8bac3cb6636e8791be01cb
ulkiobal <ulkiobal@users.noreply.github.com>
2020-11-11 17:23:05 +0100

    Classes y layouts de juegos creados

---
+++ app/src/main/java/com/example/taulaperidica/game/modalitats/ActivityGameEncertarNumero.java
@@ -0,0 +1,16 @@
+package com.example.taulaperidica.game.modalitats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.taulaperidica.R;
+
+public class ActivityGameEncertarNumero extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_encertar_numero);
+    }
+}
