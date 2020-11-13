101c3caf66964e646fe9d145a032b018a9b38e81
csanchezarisa <cristobal.sanchez@mataro.epiaedu.cat>
2020-11-13 09:29:52 +0100

    Menú principal preparado para personalizar colores Creada la ActivityPersonalitzarColors

---
+++ app/src/main/java/com/example/taulaperidica/personalitzar/ActivityPersonalitzarColors.java
@@ -0,0 +1,16 @@
+package com.example.taulaperidica.personalitzar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.taulaperidica.R;
+
+public class ActivityPersonalitzarColors extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personalitzar_colors);
+    }
+}
